62ab0e077995944a3661ed02c919e160ffb38f89
MazenAbe <mazen.abuelenain@gmail.com>
2025-04-23 02:05:22 -0400

    Read More button added

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,19 +3,25 @@ import React, { useState, useRef, useEffect } from 'react'
 function Popup( { selected, closePopUp } ) {
   const [isOpen, setIsOpen] = useState(false);
 
-  const [showReadMoreButton, setShowReadMoreButton] = useState(false);
+  const [showReadMore, setShowReadMore] = useState(false);
 
   const ref = useRef(null);
 
-  useEffect(() => {
-    if(ref.current) {
-        setShowReadMoreButton(
-            console.log(ref.current.scrollHeight, ref.current.clientHeight),
-            ref.current.scrollHeight !== ref.current.clientHeight
-        )
-    }
-  }, [])
-  
+//   useEffect(() => {
+//     if(ref.current) {
+//         console.log(ref.current.scrollHeight, ref.current.clientHeight)
+//         setShowReadMore(
+//             ref.current.scrollHeight !== ref.current.clientHeight
+//         )
+//     }
+//   }, [])
+
+    var showChar = 300;
+    var c = selected.Plot + '';
+
+    if(selected.Plot.length > showChar && !showReadMore){
+        c = c.substring(0, showChar).trim() + '...';
+    }  
   
   return (
     <section className='popup'>
@@ -23,11 +29,15 @@ function Popup( { selected, closePopUp } ) {
             <h2>{selected.Title} <span> {selected.Year} </span></h2>
             <p className='rating'>Rating: {selected.imdbRating}</p>
             <div className='plot'>
-                <img src={selected.Poster} />
-                <p>{selected.Plot}</p>
-                {showReadMoreButton && (
-                    <button className='readMore' onClick={() => setIsOpen(!isOpen)}>{isOpen ? 'read less...' : 'read more...'}</button>
-                )} 
+                {/* <img src={selected.Poster} /> */}
+                <p className={isOpen ? null : 'readMore'}>{c}
+                    <a className='readButton' onClick={() => setShowReadMore(!showReadMore)}> 
+                        {/* {showReadMore && (
+                            <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? 'read less...' : 'read more...'}</button>
+                        )} */}
+                        {showReadMore ? 'less' : 'more'}
+                    </a>
+                </p> 
             </div>
 
             <button className="close" onClick={closePopUp}>Close</button>
